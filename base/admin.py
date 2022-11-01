8da0ebf4565da2430e7124d0834ae8214d2f1a28
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 21:32:52 +0100

    Register Company model in admin

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Advocate
+from .models import Company, Advocate
 # Register your models here.
 
 admin.site.register(Advocate)
+admin.site.register(Company)
