d4e8b44593714349fb4c56550437b0f60eaa91a0
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 12:06:48 +0100

    DRF Connected in views to the endpoints

--- base/views.py
+++ base/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
-
+@api_view(['GET'])
 def endpoints(request):
     data = ['/advocates', 'advocates/:username']
-    return JsonResponse(data, safe=False)
+    return Response(data)
 
+@api_view(['GET'])
 def advocate_list(request):
     data = ['Preciousimo', 'Dennis', 'Jolomon']
-    return JsonResponse(data, safe=False)
+    return Response(data)
 
+@api_view(['GET'])
 def advocate_detail(request, username):
     data = username
-    return JsonResponse(data, safe=False)
+    return Response(data)
