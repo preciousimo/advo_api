adc814ddb6205348f99c046f03e74b919d4e4da4
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 12:19:35 +0100

    Create Advocate model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Advocate(models.Model):
+    username = models.CharField(max_length=200)
+    bio = models.TimeField(max_length=250, null=True, blank=True)
