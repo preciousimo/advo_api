b49a2fb10e6c5f42541df44c16d6245cbd0b7461
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 12:46:53 +0100

    String representation of Advocate class

--- base/models.py
+++ base/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Advocate(models.Model):
     username = models.CharField(max_length=200)
     bio = models.TextField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
