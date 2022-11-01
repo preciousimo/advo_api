8da317524aa5a28ebeac2bd99a3095693cb71723
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 17:41:56 +0100

    Class based url path

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #     """ Class based view URL"""
     path('', views.endpoints),
     path('advocates', views.advocate_list, name='advocates'),
-    path('advocates/<str:username>/', views.advocate_detail),
+    # path('advocates/<str:username>/', views.advocate_detail),
+
+    #    """ Class based view URL"""
+    path('advocates/<str:username>/', views.AdvocateDetail.as_view()),
+
 ]
