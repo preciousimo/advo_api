76f5834a3fc925795b05bd1e5cd9c5f948ab23e8
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 22:07:48 +0100

    URL endpoint for list of companies

--- base/urls.py
+++ base/urls.py
@@ -5,13 +5,14 @@ urlpatterns = [
                        # ADVOCATES
     #     """ Class based view URL"""
     path('', views.endpoints),
-    path('advocates', views.advocate_list, name='advocates'),
+    path('advocates/', views.advocate_list, name='advocates'),
     # path('advocates/<str:username>/', views.advocate_detail),
 
     #    """ Class based view URL"""
     path('advocates/<str:username>/', views.AdvocateDetail.as_view()),
 
                         # COMPANIES
+    path('companies/', views.companies_list, name='companies'),
     
 
 ]
