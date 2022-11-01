ae9b804e8da09f9de9210c8f6d28e328aa08aa49
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 21:31:03 +0100

    Create Company model

--- base/models.py
+++ base/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.TextField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Advocate(models.Model):
     username = models.CharField(max_length=200)
     bio = models.TextField(max_length=250, null=True, blank=True)
