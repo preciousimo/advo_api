f95c3580e716341713e1e9700dd3d406976c43d1
preciousimo <preciousimoniakemu@gmail.com>
2022-11-02 00:27:26 +0100

    Include TokenRefrehView

--- base/urls.py
+++ base/urls.py
@@ -3,20 +3,24 @@ from . import views
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
+    TokenRefreshView,
 )
 
 urlpatterns = [
                        # ADVOCATES
-    #     """ Class based view URL"""
+    #     """ Function based view URL """
     path('', views.endpoints),
     path('advocates/', views.advocate_list, name='advocates'),
     # path('advocates/<str:username>/', views.advocate_detail),
 
-    #    """ Class based view URL"""
+    #    """ Class based view URL """
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('advocates/<str:username>/', views.AdvocateDetail.as_view()),
 
                         # COMPANIES
+    #     """ Function based view URL """
     path('companies/', views.companies_list, name='companies'),
     
 
