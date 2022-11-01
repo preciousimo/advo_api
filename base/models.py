6b15261c866b1c4a89793ea9f3bed09e829e8dda
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 22:50:48 +0100

    Create Company <-> Advocate relationship in model

--- base/models.py
+++ base/models.py
@@ -10,6 +10,7 @@ class Company(models.Model):
         return self.name
 
 class Advocate(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
     username = models.CharField(max_length=200)
     bio = models.TextField(max_length=250, null=True, blank=True)
 
