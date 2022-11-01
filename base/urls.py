eaf0d48d72979e8914bec11e13553e8c1f3efe6d
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 23:54:32 +0100

    Get authenticated with simple JWT

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
 urlpatterns = [
                        # ADVOCATES
     #     """ Class based view URL"""
@@ -9,6 +13,7 @@ urlpatterns = [
     # path('advocates/<str:username>/', views.advocate_detail),
 
     #    """ Class based view URL"""
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('advocates/<str:username>/', views.AdvocateDetail.as_view()),
 
                         # COMPANIES
