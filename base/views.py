34f5c797e4ec87cf176e6bf85b05975027c28909
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 12:55:34 +0100

    Call unserialized advocate objects in views

--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Advocate
 
 # Create your views here.
 @api_view(['GET'])
@@ -11,8 +14,9 @@ def endpoints(request):
 
 @api_view(['GET'])
 def advocate_list(request):
-    data = ['Preciousimo', 'Dennis', 'Jolomon']
-    return Response(data)
+    # data = ['Preciousimo', 'Dennis', 'Jolomon']
+    advocates = Advocate.objects.all()
+    return Response(advocates)
 
 @api_view(['GET'])
 def advocate_detail(request, username):
