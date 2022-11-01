a066f33f6c65d7c45e849b6049a2988d46b9c8ad
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 12:44:38 +0100

    Change bio Field to TextField

--- base/models.py
+++ base/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Advocate(models.Model):
     username = models.CharField(max_length=200)
-    bio = models.TimeField(max_length=250, null=True, blank=True)
+    bio = models.TextField(max_length=250, null=True, blank=True)
