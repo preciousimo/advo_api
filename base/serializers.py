8e642df8bf01c29b52a356cdc8682312e527d46d
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 23:07:54 +0100

    Many to many relationship with Company & Advocate

--- base/serializers.py
+++ base/serializers.py
@@ -1,12 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Advocate, Company
 
-class AdvocateSerializer(ModelSerializer):
-    class Meta:
-        model = Advocate
-        fields = '__all__'
 
 class CompanySerializer(ModelSerializer):
+    employee_count = SerializerMethodField(read_only=True)
     class Meta:
         model = Company
         fields = '__all__'
+
+    def get_employee_count(self, obj):
+        count = obj.advocate_set.count()
+        return count
+
+class AdvocateSerializer(ModelSerializer):
+    company = CompanySerializer()
+    class Meta:
+        model = Advocate
+        fields =  ['username', 'bio', 'company']
