34c691dd15b8846a8e8707f278224dabc3dfee3b
preciousimo <preciousimoniakemu@gmail.com>
2022-11-01 11:00:04 +0100

    Create initial views for endpoint

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def endpoints(request):
+    data = ['/advocates', 'advocates/:username']
+    return JsonResponse(data, safe=False)
