e8410a963d4aee2b6b3596ad034731f545fa29e6
preciousimo <preciousimoniakemu@gmail.com>
2022-11-03 22:40:56 +0100

    Update DB

--- advo_api/settings.py
+++ advo_api/settings.py
@@ -131,7 +131,7 @@ WSGI_APPLICATION = 'advo_api.wsgi.application'
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'), 
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
