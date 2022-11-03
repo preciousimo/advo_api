626ee5471e9cc0310156c82b098137110a4d05eb
preciousimo <preciousimoniakemu@gmail.com>
2022-11-03 23:09:24 +0100

    Configure staticfiles properly

--- advo_api/settings.py
+++ advo_api/settings.py
@@ -181,7 +181,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
