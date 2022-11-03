2349bf520aeccd58a92e8ba31cef122eacf835d7
preciousimo <preciousimoniakemu@gmail.com>
2022-11-03 22:49:01 +0100

    Update requirement file

--- advo_api/settings.py
+++ advo_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from datetime import timedelta
+import psycopg2
 import dj_database_url
 from decouple import config, Csv 
 from dotenv import load_dotenv
