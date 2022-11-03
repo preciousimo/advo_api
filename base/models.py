5905a13e97b9405db2178ad6b9e1900f33a88c83
preciousimo <preciousimoniakemu@gmail.com>
2022-11-03 16:36:51 +0100

    Modify advocate model

--- base/models.py
+++ base/models.py
@@ -11,8 +11,11 @@ class Company(models.Model):
 
 class Advocate(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
-    username = models.CharField(max_length=200)
-    bio = models.TextField(max_length=250, null=True, blank=True)
+    name = models.CharField(max_length=500, null=True, blank=True)
+    profile_pic = models.CharField(max_length=500, null=True, blank=True)
+    username = models.CharField(max_length=500, null=True, blank=True)
+    bio = models.TextField(max_length=550, null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.username
