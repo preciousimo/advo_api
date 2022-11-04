7eedf573d866dd7d44790b9b45be26b343452862
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 11:45:33 +0100

    Setup AWS S3 storage

--- advo_api/settings.py
+++ advo_api/settings.py
@@ -192,6 +192,14 @@ STATICFILES_DIRS = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
+AWS_QUERYSTRING_AUTH = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_ACCESS_KEY_ID = str(os.getenv('AWS_S3_ACCESS_KEY_ID'))
+AWS_S3_SECRET_ACCESS_KEY = str(os.getenv('AWS_S3_SECRET_ACCESS_KEY'))
+
+AWS_STORAGE_BUCKET_NAME = 'advo'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-a1a1.up.railway.app']
