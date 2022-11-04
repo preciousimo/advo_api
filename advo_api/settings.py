d60de7dfccfbc56807f90e683fe792c54eb28746
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 11:48:25 +0100

    Update requirement file

--- advo_api/settings.py
+++ advo_api/settings.py
@@ -184,7 +184,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
