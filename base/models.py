1a0de7af2111b4cce16a0cc51b30fd5e2feb241f
preciousimo <preciousimoniakemu@gmail.com>
2022-11-04 11:36:15 +0100

    Update Advocate model field

--- base/models.py
+++ base/models.py
@@ -12,7 +12,7 @@ class Company(models.Model):
 class Advocate(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=500, null=True, blank=True)
-    profile_pic = models.CharField(max_length=500, null=True, blank=True)
+    profile_pic = models.ImageField(null=True, blank=True)
     username = models.CharField(max_length=500, null=True, blank=True)
     bio = models.TextField(max_length=550, null=True, blank=True)
     twitter = models.URLField(null=True, blank=True)
